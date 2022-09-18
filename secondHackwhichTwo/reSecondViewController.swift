6a1d1d92dfdfae72fb6e4087d44d0c9a77a79d35
mehranHAWAII <mehranh@hawaii.edu>
2022-09-17 22:26:12 -1000

    Second Completion Part 4-6

---
+++ secondHackwhichTwo/reSecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  reSecondViewController.swift
+//  secondHackwhichTwo
+//
+//  Created by Mehran Hashemi on 9/17/22.
+//  Copyright © 2022 Mehran Hashemi. All rights reserved.
+//
+
+import UIKit
+
+class reSecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func ChangeColorButtonPressed(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
