f53bb348bafac307011c047d27f51979e7a8e77b
mehranHAWAII <mehranh@hawaii.edu>
2022-09-17 20:46:28 -1000

    Second Added a button to the viewcontroller

---
+++ secondHackwhichTwo/secondViewControllerTwo.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewControllerTwo.swift
+//  secondHackwhichTwo
+//
+//  Created by Mehran Hashemi on 9/17/22.
+//  Copyright © 2022 Mehran Hashemi. All rights reserved.
+//
+
+import UIKit
+
+class secondViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
